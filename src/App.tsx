b3b0935341d1231e423c4fe856c9239593a3b073
Stepan Sych <stepansych@gmail.com>
2022-05-27 16:51:26 +0300

    Add data and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {FaFacebook, FaInstagram, FaTelegram, FaTwitter, FaViber, FaYoutube} from "react-icons/fa";
+import './App.css';
+import CurrentDate from "./components/CurrentDate";
 
-function App() {
-  return (
-    <div className="App">
-      <p>Test</p>
-    </div>
-  );
+const App = () => {
+
+    return (
+        <div className="App">
+            <header className="Header">
+                <CurrentDate/>
+                <div className="App-icons">
+                    <a className="Icon" href="https://facebook.com/"><FaFacebook size={20}/></a>
+                    <a className="Icon" href="https://youtube.com/"><FaYoutube size={20}/></a>
+                    <a className="Icon" href="https://instagram.com/"><FaInstagram size={20}/></a>
+                    <a className="Icon" href="https://telegram.com/"><FaTelegram size={20}/></a>
+                    <a className="Icon" href="https://viber.com/"><FaViber size={20}/></a>
+                    <a className="Icon" href="https://twitter.com/"><FaTwitter size={20}/></a>
+                </div>
+            </header>
+            <div className="App-background-image">
+            </div>
+        </div>
+    );
 }
 
 export default App;
